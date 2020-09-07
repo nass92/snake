e70e738c75d638604fcb7c03242ffa9e5cf8b26e
nass92 <70922448+nass92@users.noreply.github.com>
2020-09-08 01:37:29 +0200

    Add files via upload

---
+++ snake/snake.js
@@ -0,0 +1,52 @@
+function Snake() {
+this.x = 0;
+this.y = 0;
+this.xSpeed = scale*1;
+this.ySpeed = 0
+
+this.draw= function() {
+    ctx.fillStyle = "#FFFFFF"
+    ctx.fillRect(this.x, this.y, scale, scale)}
+
+this.update = function(){
+   this.x += this.xSpeed;
+   this.y += this.ySpeed; 
+    if (this.x > canvas.width) {
+        this.x = 0;}
+    if (this.y > canvas.height) {
+        this.x = 0;}
+    if (this.x < 0) {
+        this.x = canvas.width;}
+    if (this.y < 0) {
+        this.y = canvas.height;}
+}
+
+this.changeDirection = function (direction){
+    switch(direction){
+        case 'Up': 
+            this.xSpeed = 0;
+            this.ySpeed = -scale *1;
+            break;
+        case 'Down': 
+            this.xSpeed = 0;
+            this.ySpeed = scale *1;
+            break;
+        case 'Left': 
+            this.xSpeed = -scale *1;
+            this.ySpeed = 0;
+            break;
+        case 'Right': 
+            this.xSpeed =  scale *1;
+            this.ySpeed = 0;
+            break;
+
+       
+    }
+}
+this.eat = function(fruit) {
+   
+    if (this.x===fruit.x && this.y===fruit.y){return true 
+    }
+    return false; 
+}
+}
